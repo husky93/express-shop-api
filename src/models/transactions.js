0b69cf7012df00ba0bfd07af750a408068247806
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-12 19:12:44 +0100

    Create initial Transactions model

--- src/models/transactions.js
+++ src/models/transactions.js
@@ -2,6 +2,18 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const TransactionsSchema = new Schema({});
+const TransactionsSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: 'Users' },
+  items: [
+    {
+      item: { type: Schema.Types.ObjectId, ref: 'Irems' },
+      quantity: Number,
+    },
+  ],
+  status: {
+    type: String,
+    enum: ['pending', 'delivered', 'payment failed', 'cancelled'],
+  },
+});
 
 module.exports = mongoose.model('Transactions', TransactionsSchema);
