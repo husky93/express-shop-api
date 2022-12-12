a3bf596264a7cba7ae27ae6e45fc16aa84cb1b37
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-12 19:15:22 +0100

    Add item id that is reviewed to Reviews model

--- src/models/reviews.js
+++ src/models/reviews.js
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 
 const ReviewsSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: 'Users', required: true },
+  item: { type: Schema.Types.ObjectId, ref: 'Items', required: true },
   rating: { type: Number, min: 1, max: 5, required: true },
   description: String,
 });
