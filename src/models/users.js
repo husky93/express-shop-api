8845db52e5f6747c2a9b06fae47e24b2436866ef
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-12 18:56:11 +0100

    Create initial Users model

--- src/models/users.js
+++ src/models/users.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const UserSchema = new Schema({});
+const UserSchema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  transactions: [{ type: Schema.Types.ObjectId, ref: 'Transactions' }],
+  address: { type: Schema.Types.ObjectId, ref: 'Address' },
+});
 
 module.exports = mongoose.model('Users', UserSchema);
