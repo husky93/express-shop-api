9963cd7cf5549ec2dfaed96c50d0e4d7ccf2866d
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-12 18:51:08 +0100

    Create initial Categories model

--- src/models/categories.js
+++ src/models/categories.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const CategoriesSchema = new Schema({});
+const CategoriesSchema = new Schema({
+  title: { type: String, required: true, maxLength: 150 },
+  description: { type: String, required: true },
+});
 
 module.exports = mongoose.model('Categories', CategoriesSchema);
