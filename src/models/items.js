98aed40ca5e9874dd92ad86e36c40e368c4655a4
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-12 18:52:56 +0100

    Create initial Items model

--- src/models/items.js
+++ src/models/items.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const ItemsSchema = new Schema({});
+const ItemsSchema = new Schema({
+  title: { type: String, required: true, maxLength: 150 },
+  description: { type: String, required: true },
+  category: { type: Schema.Types.ObjectId, ref: 'Categories' },
+  price: { type: Number, required: true },
+  num_in_stock: { type: Number, required: true },
+});
 
 module.exports = mongoose.model('Items', ItemsSchema);
