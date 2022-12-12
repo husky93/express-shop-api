3f49d16d31b1b9e54bcb01c6c0a1d11519d9f94e
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-12 19:01:17 +0100

    Create initial Address model

---
+++ src/models/address.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const AddressSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: 'Users' },
+  city: { type: String, required: true, maxLength: 150 },
+  zip_code: { type: String, required: true, match: /^[0-9]{2}-[0-9]{3}/ },
+  street: { type: String, required: true, maxLength: 150 },
+  house_num: { type: String, required: true, maxLength: 150 },
+});
+
+module.exports = mongoose.model('Address', AddressSchema);
