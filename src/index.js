780ecfd87dfd0305badde7b96f5cf12e188c2b4e
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-13 15:12:41 +0100

    Connect application to database

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 import routes from './routes';
+import mongoose from 'mongoose';
+
+const mongoDB = process.env.MONGODB_URL;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/transactions', routes.transactions);
 app.use('/categories', routes.categories);
