c61214b79eb7fae3f8f3d3c4287a3abf17cb4714
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-14 15:18:50 +0100

    Send all items as a JSON response with option to filter by category

--- src/controllers/itemsController.js
+++ src/controllers/itemsController.js
@@ -1,35 +1,103 @@
-exports.getItems = (req, res) => {
-  res.json({ posts: 'YO' });
+import async from 'async';
+import Categories from '../models/categories';
+import Items from '../models/items';
+
+const getItems = (req, res, next) => {
+  const { category } = req.query;
+
+  if (category) {
+    async.waterfall(
+      [
+        function (callback) {
+          Categories.findOne({ title: category.toLowerCase() }).exec(
+            (err, categoryData) => {
+              if (err) {
+                return callback(err, null);
+              }
+              return callback(null, categoryData);
+            }
+          );
+        },
+        function (categoryData, callback) {
+          if (categoryData) {
+            const id = categoryData._id;
+            Items.find({ category: id })
+              .populate('category')
+              .exec((err, itemsList) => {
+                if (err) {
+                  return callback(err, null);
+                }
+                return callback(null, itemsList);
+              });
+          } else {
+            return callback(
+              res.status(400).json({ error: 'Category not found.' }),
+              null
+            );
+          }
+        },
+      ],
+      (err, results) => {
+        if (err) {
+          return next(err);
+        }
+        if (results.length > 0) return res.json(results);
+        return res.status(400).json({ error: 'No items found.' });
+      }
+    );
+  } else {
+    Items.find()
+      .populate('category')
+      .exec((err, itemsList) => {
+        if (err) {
+          return next(err);
+        }
+        if (itemsList.length > 0) return res.json(itemsList);
+        return res.status(400).json({ error: 'No items found.' });
+      });
+  }
 };
 
-exports.getItem = (req, res) => {
+const getItem = (req, res) => {
   res.json({ post: 'GET' });
 };
 
-exports.postItem = (req, res) => {
+const postItem = (req, res) => {
   res.json({ post: 'POSTED' });
 };
 
-exports.updateItem = (req, res) => {
+const updateItem = (req, res) => {
   res.json({ post: 'YO' });
 };
 
-exports.deleteItem = (req, res) => {
+const deleteItem = (req, res) => {
   res.json({ post: 'YO' });
 };
 
-exports.getReviews = (req, res) => {
+const getReviews = (req, res) => {
   res.json({ post: 'YO' });
 };
 
-exports.postReview = (req, res) => {
+const postReview = (req, res) => {
   res.json({ post: 'YO' });
 };
 
-exports.updateReview = (req, res) => {
+const updateReview = (req, res) => {
   res.json({ post: 'YO' });
 };
 
-exports.deleteReview = (req, res) => {
+const deleteReview = (req, res) => {
   res.json({ post: 'YO' });
+};
+
+export default {
+  getItems,
+  getItem,
+  postItem,
+  updateItem,
+  deleteItem,
+  getReviews,
+  postReview,
+  updateReview,
+  deleteReview,
 };
