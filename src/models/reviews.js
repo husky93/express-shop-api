0286ccb3af8b6265e91c470e39d2ea8498eef993
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-15 15:52:24 +0100

    Add timestamp to reviews model

--- src/models/reviews.js
+++ src/models/reviews.js
@@ -6,6 +6,7 @@ const ReviewsSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: 'Users', required: true },
   item: { type: Schema.Types.ObjectId, ref: 'Items', required: true },
   rating: { type: Number, min: 1, max: 5, required: true },
+  timestamp: { type: Date, default: Date.now() },
   description: String,
 });
 
