bd610ca340d4ca7a852968aed9e78a85a06b19a3
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-16 10:50:23 +0100

    Create new category on POST request that passes validation

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,4 +1,7 @@
+import async from 'async';
+import { body, validationResult } from 'express-validator';
 import Categories from '../models/categories';
+import Items from '../models/items';
 
 const getCategories = (req, res, next) => {
   Categories.find()
@@ -20,9 +23,38 @@ const getCategory = (req, res, next) => {
   });
 };
 
-const postCategory = (req, res) => {
-  res.json({ post: 'POSTED' });
-};
+const postCategory = [
+  body('title')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Title must be specified.')
+    .isLength({ max: 150 })
+    .withMessage('Title must be maximum 150 characters.'),
+  body('description')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Message must have at least 1 character.'),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors);
+    }
+
+    const category = new Categories({
+      title: req.body.title,
+      description: req.body.description,
+    });
+
+    category.save((error, result) => {
+      if (error) {
+        return res.status(500).json(errors);
+      }
+      return res.json(result);
+    });
+  },
+];
 
 const updateCategory = (req, res) => {
   res.json({ post: 'YO' });
