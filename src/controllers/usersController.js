c9f001dc81a12458d79db3f103a6d8f45eac1166
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-16 12:48:51 +0100

    Send single user data to client on correct GET request

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,18 +1,14 @@
-import { nextTick } from 'async';
 import { body, validationResult } from 'express-validator';
 import Users from '../models/users';
 
 const getUser = (req, res, next) => {
-  Users.findById(req.params.categoryId)
-    .select('+password')
-    .exec((err, user) => {
-      if (err) {
-        next(err);
-      }
-      if (user == null)
-        return res.status(404).json({ error: 'User not found' });
-      return res.json(user);
-    });
+  Users.findById(req.params.userId).exec((err, user) => {
+    if (err) {
+      next(err);
+    }
+    if (user == null) return res.status(404).json({ error: 'User not found' });
+    return res.json(user);
+  });
 };
 
 const postUser = (req, res) => {
