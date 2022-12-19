5e731732a8b7ea8a334cf1cac0196e0201723241
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-19 11:13:23 +0100

    Setup cors for development

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
