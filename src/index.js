1782260109decb3093b334560da081f3c3e09703
Maciej Sroka <sroka.maciej93@gmail.com>
2023-01-02 18:07:51 +0100

    Add Admin Dashboard website url as a CORS option

--- src/index.js
+++ src/index.js
@@ -13,7 +13,12 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(
+  cors({
+    origin: 'https://husky93.github.io/ecommerce-admin-frontend/',
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
