8c7624e33e02ef874575bbdeadbbe2f8e595fab0
Maciej Sroka <sroka.maciej93@gmail.com>
2023-01-13 15:21:56 +0100

    Add images route to index routes file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import user from './user';
 import auth from './auth';
 import items from './items';
+import images from './images';
 import categories from './categories';
 import transactions from './transactions';
 
@@ -8,6 +9,7 @@ export default {
   user,
   auth,
   items,
+  images,
   categories,
   transactions,
 };
